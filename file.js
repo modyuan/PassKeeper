7937c2119b2319ddb390d6d979319d9b164a1e74
yuansu <yzmoe@outlook.com>
2018-07-30 01:58:12 +0800

    add function: 	load,save,update

---
+++ file.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const pathOfFile = process.env.npm_package_config_path;
+
+module.exports = {
+    load: function () {
+        return new Promise(function (resolve, reject) {
+            fs.readFile(pathOfFile, {encoding: "utf-8"}, (err, data) => {
+                if (err) {//文件不存在就创建新的。
+
+                    fs.writeFile(pathOfFile,'[]',(err)=>{
+                        if(err){
+                            reject(err);
+                        } else{
+                            resolve('[]');
+                        }
+                    });
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    },
+    save: function (ob) {
+        return new Promise((resolve, reject) => {
+            let obs = JSON.stringify(ob);
+            fs.writeFile(pathOfFile + ".temp",obs,{flag:"w"},(err)=>{
+                if (err) {
+                    reject(err);
+                }else{
+                    fs.unlink(pathOfFile, (err) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            fs.rename(pathOfFile+ ".temp", pathOfFile, (err) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                        }
+                    })
+                }
+            });
+        });
+
+    }
+
+};
--- main.js
+++ main.js
@@ -2,13 +2,39 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-var window = null;
+app.commandLine.appendSwitch('remote-debugging-port', '9223');
 
+const file = require('./file');
+
+let window = null;
+let ipcMain = electron.ipcMain;
+
+ipcMain.on("loadFile",function(event,arg){
+    console.log("event: loadFile.");
+    file.load().then((data)=>{
+        let countTable=JSON.parse(data);
+        event.sender.send("loadFile-reply",countTable);
+    })
+        .catch(()=>{
+            event.sender.send("failToLoad");
+        });
+});
+
+ipcMain.on("saveFile",(event,arg)=>{
+    console.log("event: saveFile.");
+    file.save(arg).then().catch(()=>{
+        event.sender.send("failToSave");
+    })
+});
 
 
 app.on('ready', function() {
     window = new BrowserWindow({width: 500, height: 400,icon : __dirname+"/imgs/icon.ico"});
     window.loadURL('file://' + __dirname + '/index.html');
     //window.webContents.openDevTools();
-    window.setMenu(null)
+    window.setMenu(null);
+
+
+
+
 });
